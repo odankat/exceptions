74db2e05126c57f0fa94e714afce1b243944524e
HomeMedia <taknado13@yandex.ru>
2024-05-11 17:05:26 +0700

    start

---
+++ src/main/java/skypro/ru/register/ExceptionsApplication.java
@@ -0,0 +1,13 @@
+package skypro.ru.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skypro/ru/register/ExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.ru.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
